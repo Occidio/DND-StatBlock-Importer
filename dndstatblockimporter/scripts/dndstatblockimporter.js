111687e9b04aadc80415bc10a5aa2b8cb856450b
Alex Bamford <Occidio001@gmail.com>
2021-02-13 16:10:48 +0000

    Add hooks for testing.

--- dndstatblockimporter/scripts/dndstatblockimporter.js
+++ dndstatblockimporter/scripts/dndstatblockimporter.js
@@ -12,6 +12,31 @@ export class statblockImportApp {
     }
 }
 
+// Run when Foundry gets initialized
+Hooks.once("init", async function() { 
+
+    /* Why did i stop the enter propagation in the old sbc?
+    window.addEventListener("keydown",function(e) {
+        if(e.keyIdentifier=="U+000A" || e.keyIdentifier=="Enter") {
+            if(e.target.id=="sbcInput") {
+                e.stopPropagation()
+                return false
+            }
+        }
+    },true);
+    */
+   
+    
+});
+
+Hooks.once("setup", function() {
+    console.log("Setup")
+});
+
+Hooks.once("ready", async function() {
+    console.log("ready")
+});
+
 Hooks.on("renderActorDirectory", (app, html, data) => {
     const startImportButton = $("<button id='startImportButton' class='create-entity 5eimportButton'><i class='fas fa-file-import'></i></i>Convert Statblock</button>");
     html.find(".directory-footer").append(startImportButton)
