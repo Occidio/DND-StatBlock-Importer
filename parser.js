e096176c75830e3a4eb5343f2cd6ef7a26e27221
Alex Bamford <Occidio001@gmail.com>
2021-02-13 10:53:49 +0000

    Improve pattern matching

--- parser.js
+++ parser.js
@@ -1,79 +1,154 @@
 const sections = ["base", "features", "actions"]
-const parsedData = {}
+let parsedData = {}
 const baseKeywords = ["Armor Class", "Hit Points", "Speed", "STR", "DEX", "CON", "INT", "WIS", "CHA", "Damage Immunities", "Condition Immunities", "Senses", "Languages", "Challenge"]
 
 function format() {
-    var inputElement = document.getElementById('inputStatBlock');
+  var inputElement = document.getElementById('inputStatBlock');
+  parsedData = {}
 
-    var noBaseInput = parseBase(inputElement.value);
-    // var featuresResult, noFeaturesInput = parseFeatures(noBaseInput);
-    // var actionResult = parseActions(noFeaturesInput);
-    console.log(noBaseInput)
+  parseBase(inputElement.value);
+  // var featuresResult, noFeaturesInput = parseFeatures(noBaseInput);
+  // var actionResult = parseActions(noFeaturesInput);
+  // console.log(noBaseInput)
 }
 
-function parseBase(inputStatBlock) {
-    //name
-    parsedData.name = parseName(inputStatBlock);
+function matchFollowingDigits(preceedingString, fullBase) {
+  let pattern = preceedingString + "[\\n| ](\\d+)"
+  let match = new RegExp(pattern).exec(fullBase)
 
-    //AC "Armor Class (\d+)"
-    let acRegex = /Armor Class (\d+)/g
-    parsedData.ac = acRegex.exec(inputStatBlock)[1];
-
-    //HP "Hit Points (\d+)"
-    let hpRegex = /Hit Points (\d+)/g
-    parsedData.hp = hpRegex.exec(inputStatBlock)[1];
- 
-    //Speed "Speed (\d+)"
-    let speedRegex = /Speed (\d+)/g
-    parsedData.speed = speedRegex.exec(inputStatBlock)[1];
-
-    //Attr "STR\n(\d+)"
-    parsedData.attr = {};
-    parsedData.attr.str = /STR\n(\d+)/g.exec(inputStatBlock)[1];
-    parsedData.attr.dex = /DEX\n(\d+)/g.exec(inputStatBlock)[1];
-    parsedData.attr.con = /CON\n(\d+)/g.exec(inputStatBlock)[1];
-    parsedData.attr.wis = /WIS\n(\d+)/g.exec(inputStatBlock)[1];
-    parsedData.attr.int = /INT\n(\d+)/g.exec(inputStatBlock)[1];
-    parsedData.attr.cha = /CHA\n(\d+)/g.exec(inputStatBlock)[1];
-
-    //Saving Throws "Saving Throws\s([a-zA-Z0-9]+.*,\s[a-zA-Z0-9]+.*)""
-    // parsedData.savingthrows = {}
-    
-    // let stRegex = /Saving Throws\s([a-zA-Z0-9]+.*,\s[a-zA-Z0-9]+.*)/g.exec(inputStatBlock)[1];
-    // let specificThrowRegex = /(\w{3})\s\+(\d+)/g.exec(stRegex);
-    // parsedData.savingthrows.specificThrowRegex[1] = specificThrowRegex[2]
-
-    //Skills "Skills\s(\w* \+\d+)(,\s*\w* \+\d+)*"
-
-    //DR split(',')
-
-    //DI split(',')
-
-    //CI split(',')
-
-    //Senses "Senses\s([a-zA-Z0-9]+.*,\s[a-zA-Z0-9]+.*)" then split(,)
-
-    //Languages split(',')
-
-    //Challenge "Challenge\s(\d+)"
-
-    // var indexOfLastBracketInChallenge = keywordPositions["Challenge"] + inputStatBlock.substring(keywordPositions["Challenge"], inputStatBlock.length).indexOf(')') + 1;
-
-    // return inputStatBlock.substring(indexOfLastBracketInChallenge, inputStatBlock.length);
-}
-
-function compare( a, b ) {
-    if ( a.index < b.index ){
-      return -1;
-    }
-    if ( a.index > b.index ){
-      return 1;
-    }
-    return 0;
+  if (match) {
+    return match[1]
+  } else {
+    console.log("Could not find " + preceedingString)
   }
-  
+}
 
-function parseName(inputStatBlock){
-    var indexOfFirstNewLine = inputStatBlock.indexOf("\n");
-    return inputStatBlock.slice(0,indexOfFirstNewLine).trim();
+function matchCommaListOfString(preceedingString, fullBase) {
+  let pattern = preceedingString + "\\s([a-zA-Z]+.*(?:,\\s[a-zA-Z]+.*)?|\\n)"
+  let match = new RegExp(pattern).exec(fullBase)
+
+  if (match) {
+    return match[1].split(',')
+  } else {
+    console.log("Could not find " + preceedingString)
+  }
+}
+
+//I hope you like RegExp
+function parseBase(inputStatBlock) {
+  //name
+  parsedData["name"] = /^(.*)/g.exec(inputStatBlock)[0]
+
+  //AC "Armor Class (\d+)"
+  parsedData["ac"] = matchFollowingDigits("Armor Class", inputStatBlock)
+
+  //HP "Hit Points (\d+)"
+  parsedData["hp"] = matchFollowingDigits("Hit Points", inputStatBlock)
+
+  //Speed "Speed (\d+)"
+  parsedData["speed"] = matchFollowingDigits("Speed", inputStatBlock)
+
+  //Attr "STR\n(\d+)"
+  parsedData.attr = {};
+  parsedData.attr["str"] = matchFollowingDigits("STR", inputStatBlock)
+  parsedData.attr["dex"] = matchFollowingDigits("DEX", inputStatBlock)
+  parsedData.attr["con"] = matchFollowingDigits("CON", inputStatBlock)
+  parsedData.attr["wis"] = matchFollowingDigits("WIS", inputStatBlock)
+  parsedData.attr["int"] = matchFollowingDigits("INT", inputStatBlock)
+  parsedData.attr["cha"] = matchFollowingDigits("CHA", inputStatBlock)
+
+  //Saving Throws "Saving Throws\s([a-zA-Z]+.*,\s[a-zA-Z]+.*)"
+  specificThrows = matchCommaListOfString("Saving Throws", inputStatBlock)
+
+  if (specificThrows) {
+    parsedData.savingthrows = {}
+    specificThrows.forEach(function (savingThrow, index) {
+      specificThrowMatch = /(\w+)\s(\+|\-)(\d+)/g.exec(savingThrow);
+      parsedData.savingthrows[index] = {}
+      parsedData.savingthrows[index].attr = specificThrowMatch[1]
+      parsedData.savingthrows[index].sign = specificThrowMatch[2]
+      parsedData.savingthrows[index].value = specificThrowMatch[3]
+    })
+  }
+
+  //Skills "Skills\s(\w+)\s\+(\d+)[,|\n]\s(\w+)\s\+(\d+)"
+  specificSkills = matchCommaListOfString("Skills", inputStatBlock)
+
+  if (specificSkills) {
+    parsedData.skills = {}
+    specificSkills.forEach(function (skill, index) {
+      skillMatch = /(\w+)\s(\+|\-)(\d+)/g.exec(skill);
+      parsedData.skills[index] = {}
+      parsedData.skills[index].skill = skillMatch[1]
+      parsedData.skills[index].sign = skillMatch[2]
+      parsedData.skills[index].value = skillMatch[3]
+    })
+  }
+
+  //DR split(',')
+  specificDR = matchCommaListOfString("Damage Resistances", inputStatBlock)
+
+  if (specificDR) {
+    parsedData.DamageResistances = {}
+    specificDR.forEach(function (dr, index) {
+      parsedData.DamageResistances[index] = dr.trim()
+    })
+  }
+
+  //DI split(',')
+  specificDI = matchCommaListOfString("Damage Immunities", inputStatBlock)
+
+  if (specificDI) {
+    parsedData.damageImmunities = {}
+    specificDI.forEach(function (di, index) {
+      parsedData.damageImmunities[index] = di.trim()
+    })
+  }
+
+  //CI split(',')
+  specificCI = matchCommaListOfString("Condition Immunities", inputStatBlock)
+
+  if (specificCI) {
+    parsedData.conditionImmunities = {}
+    specificCI.forEach(function (ci, index) {
+      parsedData.conditionImmunities[index] = ci.trim()
+    })
+  }
+
+  //Senses "Senses\s([a-zA-Z0-9]+.*,\s[a-zA-Z0-9]+.*)" then split(,)
+  specificSenses = matchCommaListOfString("Senses", inputStatBlock)
+
+  if (specificSenses) {
+    parsedData.senses = {}
+    specificSenses.forEach(function (sense, index) {
+      parsedData.senses[index] = sense.trim()
+    })
+  }
+
+  //Languages split(',')
+  specificLang = matchCommaListOfString("Languages", inputStatBlock)
+
+  if (specificLang) {
+    parsedData.languages = {}
+    specificLang.forEach(function (language, index) {
+      parsedData.languages[index] = language.trim()
+    })
+  }
+
+  //Challenge "Challenge\s(\d+)"
+  parsedData.cr = matchFollowingDigits("Challenge", inputStatBlock)
+
+  // var indexOfLastBracketInChallenge = keywordPositions["Challenge"] + inputStatBlock.substring(keywordPositions["Challenge"], inputStatBlock.length).indexOf(')') + 1;
+
+  // return inputStatBlock.substring(indexOfLastBracketInChallenge, inputStatBlock.length);
+}
+
+function compare(a, b) {
+  if (a.index < b.index) {
+    return -1;
+  }
+  if (a.index > b.index) {
+    return 1;
+  }
+  return 0;
 }
